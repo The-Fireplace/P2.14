de39aa3e159c2870cb953f5752db52147ab16d85
The_Fireplace <the.f1repl4ce@gmail.com>
2019-11-23 16:33:01 -0600

    Finish following tutorial, begin adding our own stuff

--- js/SceneMainMenu.js
+++ js/SceneMainMenu.js
@@ -3,20 +3,45 @@ class SceneMainMenu extends Phaser.Scene {
         super({ key: "SceneMainMenu" });
     }
 
+    preload() {
+        this.load.image("sprBg", "resources/background.png");
+        this.load.image("sprBtnPlay", "resources/play_button.png");
+        this.load.image("sprLogo", "resources/SPACIALDELIVERY-1.png");
+        this.load.image("sprShip", "resources/SpaceShipWFire.png");
+    }
+
     create() {
         //this.scene.start("ScenePlay");
         this.btnPlay = this.add.sprite(
             this.game.config.width * 0.5,
-            this.game.config.height * 0.5,
+            this.game.config.height * 0.75,
             "sprBtnPlay"
+        );
+
+        this.btnShip = this.add.sprite(
+            this.game.config.width * 0.5,
+            this.game.config.height * 0.6,
+            "sprShip"
+        );
+
+        this.btnLogo = this.add.sprite(
+            this.game.config.width * 0.5,
+            this.game.config.height * 0.3,
+            "sprLogo"
         );
 
         this.backgrounds = [];
         for (let i = 0; i < 5; i++) {
-            const keys = ["sprBg", "sprBg"];
+            const keys = ["sprBg"];
             const key = keys[Phaser.Math.Between(0, keys.length - 1)];
             const bg = new ScrollingBackground(this, key, i * 10);
             this.backgrounds.push(bg);
+        }
+    }
+
+    update() {
+        for (let i = 0; i < this.backgrounds.length; i++) {
+            this.backgrounds[i].update();
         }
     }
 }
