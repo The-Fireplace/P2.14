6e2dfe41dad1e20580c4fd27c987737fc1ab8c7d
The_Fireplace <the.f1repl4ce@gmail.com>
2019-11-24 18:31:48 -0600

    Fix load order

--- js/Game.js
+++ js/Game.js
@@ -24,39 +24,3 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-
-function LocalScaleManager() {
-}
-
-LocalScaleManager.prototype = {
-    scaleSprite: function (sprite, availableSpaceWidth, availableSpaceHeight, padding, scaleMultiplier, isFullScale) {
-        let scale = this.getSpriteScale(sprite.frame.width, sprite.frame.height, availableSpaceWidth, availableSpaceHeight, padding, isFullScale);
-        sprite.setScale(scale * scaleMultiplier);
-        return scale;
-    },
-    scaleSpriteTo: function (sprite, scale) {
-        sprite.setScale(scale);
-    },
-    scaleText: function (sprite, availableSpaceWidth, availableSpaceHeight, padding, scaleMultiplier, isFullScale) {
-        let originalWidth = sprite.width;
-        let originalHeight = sprite.height;
-        let scale = this.getSpriteScale(originalWidth, originalHeight, availableSpaceWidth, availableSpaceHeight, padding, isFullScale);
-        sprite.setScale(scale * scaleMultiplier);
-    },
-    getSpriteScale: function (spriteWidth, spriteHeight, availableSpaceWidth, availableSpaceHeight, minPadding, isFullScale) {
-        let ratio = 1;
-        let currentDevicePixelRatio = window.devicePixelRatio;
-        // Sprite needs to fit in either width or height
-        let widthRatio = (spriteWidth * currentDevicePixelRatio + 2 * minPadding) / availableSpaceWidth;
-        let heightRatio = (spriteHeight * currentDevicePixelRatio + 2 * minPadding) / availableSpaceHeight;
-        if (widthRatio > 1 || heightRatio > 1) {
-            ratio = 1 / Math.max(widthRatio, heightRatio);
-        } else {
-            if (isFullScale)
-                ratio = 1 / Math.max(widthRatio, heightRatio);
-        }
-        return ratio * currentDevicePixelRatio;
-    }
-};
-
-let localScaleManager = new LocalScaleManager;
