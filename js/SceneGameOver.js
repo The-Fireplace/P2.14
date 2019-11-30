e5854691b65fca57e504928b5d3a62d845312bc1
The_Fireplace <the.f1repl4ce@gmail.com>
2019-11-30 11:55:22 -0600

    Add temporary game over logo, fill in the game over scene

--- js/SceneGameOver.js
+++ js/SceneGameOver.js
@@ -6,9 +6,46 @@ class SceneGameOver extends Phaser.Scene {
     preload()
     {
         this.load.image("sprBtnContinue", "resources/touch_to_continue.png");
+        this.load.image("sprGameOver", "resources/game_over.png");
     }
 
     create() {
+        this.btnContinue = this.add.sprite(
+          this.game.scale.width * 0.5,
+          this.game.scale.height * 0.75,
+          "sprBtnContinue"
+        );
+        this.btnContinue.scale = 1.5;
 
+        this.btnGameOver = this.add.sprite(
+          this.game.scale.width * 0.5,
+          this.game.scale.height * 0.3,
+          "sprGameOver"
+        );
+
+        this.input.on('pointerdown', function (pointer)
+        {
+            this.scene.start("ScenePlay");
+        }, this);
+        this.scale.on('resize', this.resize, this);
+        let gameWidth = this.cameras.main.width;
+        let gameHeight = this.cameras.main.height;
+        this.positionControls(gameWidth, gameHeight);
+    }
+
+    positionControls(width, height) {
+        localScaleManager.scaleSprite(this.btnContinue, width/2.15, height, 0, 1, true);
+        this.btnContinue.setPosition(width / 2, height * 0.825);
+
+        localScaleManager.scaleSprite(this.btnGameOver, width/1.50, height, 0, 1, true);
+        this.btnGameOver.setPosition(width / 2, height * 0.25);
+    }
+
+    resize(gameSize, baseSize, displaySize, resolution) {
+        let width = gameSize.width;
+        let height = gameSize.height;
+
+        this.cameras.resize(width, height);
+        this.positionControls(width, height);
     }
 }
